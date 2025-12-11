f81589590f94ffe30444d8d0a32c05cb139e30d8
Dipak Shedge <155218698+Im-Dipak-Shedge@users.noreply.github.com>
2025-12-11 23:34:54 +0530

    Create code.cpp

---
+++ 28.Find index of first occurrence in the string/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        for(int i=0;i<n;i++)
+        {
+        if(needle==haystack.substr(i,m))
+        {
+            return i;
+        }
+        }
+        return -1;
+    }
+};
