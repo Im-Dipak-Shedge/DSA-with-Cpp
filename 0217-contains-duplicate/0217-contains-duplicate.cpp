fc185988a8ea742149f602f53af8c225dc70f140
Dipak Shedge <155218698+Im-Dipak-Shedge@users.noreply.github.com>
2026-01-08 19:21:21 +0530

    Time: 54 ms (37.15%), Space: 90.8 MB (44.1%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, int> mp;
-        for (int i : nums) mp[i]++;
-        for(auto j: mp){
-            if(j.second >1) return true;
+        
+        for(auto i: nums){
+            mp[i]++;
+            if(mp[i] >1) return true;
         }
 return false;
     }
